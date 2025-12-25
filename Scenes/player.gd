9763278e67050fd072f234c213acaf31010a87c2
Sean <42walrustitan@gmail.com>
2025-12-24 18:11:50 -0800

    Basic Player Movement

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed = 400
 var screen_size
+var player_movement_vector
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,4 +11,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	player_movement_vector = Input.get_vector("Move Left","Move Right","Move Forward","Move Backward")
+	player_movement_vector = player_movement_vector.normalized()
+	
+	position += player_movement_vector * speed * delta
+	position = position.clamp(Vector2.ZERO,screen_size)
+	
 	pass
