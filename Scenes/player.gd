bc2477857b8967fef056feb05f058f41883ed0d3
Sean <42walrustitan@gmail.com>
2025-12-29 15:49:54 -0800

    Dodge

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var speed = 400
 @export var animation_player :AnimatedSprite2D
+var animation_state = "normal"
+
 var screen_size
 var player_movement_vector
 var player_movement_magnitude
@@ -9,7 +11,7 @@ var left_limit
 var right_limit
 signal hit
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 
 	
@@ -19,24 +21,42 @@ func _ready() -> void:
 	left_limit = Vector2(screen_size.x*.2, 0)
 	right_limit = Vector2(screen_size.x*.8, screen_size.y)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
+	#Process input for player movement
 	player_movement_vector = Input.get_vector("Move Left","Move Right","Move Forward","Move Backward")
 	player_movement_magnitude = player_movement_vector.length()
 	player_movement_magnitude = clamp(player_movement_magnitude , 0 ,1) 
 	player_movement_vector = player_movement_vector.normalized()
 	
-	if(player_movement_vector.x > 0.5):
-		animation_player.animation = "Right"
-	elif (player_movement_vector.x < -0.5):
-		animation_player.animation = "Left"
-	else:
-		animation_player.animation = "Centered"
+	#Dodge input
+	if (Input.is_action_pressed("Dodge")):
+		animation_state = "dodge"
+		animation_player.play("Flip")
 	
+	#Animate the ship side to side
+	if(animation_state == "normal"):
+		if(player_movement_vector.x > 0.5):
+			animation_player.animation = "Right"
+		elif (player_movement_vector.x < -0.5):
+			animation_player.animation = "Left"
+		else:
+			animation_player.animation = "Centered"
+	
+		
+		
+	#Move the player and clamp them to the bounds of the play area
 	position += player_movement_vector * speed * player_movement_magnitude * delta
 	position = position.clamp(left_limit, right_limit)
 
 #setup for player getting hit
 func _on_body_entered(body: Node2D) -> void:
 	print("Hit")
-	pass # Replace with function body.
+
+#Called when an animation for the ship is finished
+#Return to the normal state
+func _animation_finished() -> void:
+	print("Animation Stoped")
+	animation_state = "normal"
+	animation_player.stop()
+		
