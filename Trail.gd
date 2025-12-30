88a6fbdd7d79ec172563b13ec9fe741b6aa89c82
Sean <42walrustitan@gmail.com>
2025-12-29 17:27:20 -0800

    stuff

--- Trail.gd
+++ Trail.gd
@@ -1,13 +1,24 @@
 extends Line2D
 
 @export var length = 50
-
+var offset_from_parent
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	while (length > points.size()):
-		points.remove_at(49)
+	offset_from_parent = get_parent().global_position - position
+	var i = 0
+	for point in points:
+		set_point_position(i,Vector2(points[i].x,points[i].y + 500 *delta))
+		if(i<points.size()-1):
+			i+=1
+			
+	add_point(to_global(position))
+	
+	while get_point_count() > length:
+		remove_point(0)
+	
