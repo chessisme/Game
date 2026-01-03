0e5635ba0339100006313c9ac1e4e0b29c1f9f28
Sean <42walrustitan@gmail.com>
2026-01-02 17:13:14 -0800

    added weapon cooldowns

--- Scripts/PlayerWeapons.gd
+++ Scripts/PlayerWeapons.gd
@@ -3,8 +3,8 @@ extends Node2D
 @export var PrimaryWeapon : PackedScene
 @export var SecondaryWeapon : PackedScene 
 
-var primary_cooldown
-var secondary_cooldown
+var primary_cooldown = 0
+var secondary_cooldown = 0
 
 var new_projectile
 var rootNode
@@ -16,12 +16,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (Input.is_action_pressed("Fire Main Weapon")):
+	if (Input.is_action_pressed("Fire Main Weapon") and primary_cooldown <= 0):
 		print("Fire Primary")
-	if(Input.is_action_pressed("Fire Secondary Weapon")):
+	if(Input.is_action_pressed("Fire Secondary Weapon") and secondary_cooldown <=0):
 		print("Fire Secondary")
 		new_projectile = SecondaryWeapon.instantiate();
 		new_projectile.position = global_position
 		rootNode.add_child(new_projectile)
 		secondary_cooldown = new_projectile.cooldown
+	
+	if(primary_cooldown > 0):
+		primary_cooldown-= delta
+	if(secondary_cooldown > 0):
+		secondary_cooldown -= delta
 	
