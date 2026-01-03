2e465bf1f4f22e3759f99476be8c22118e50720c
Sean <42walrustitan@gmail.com>
2026-01-02 17:53:25 -0800

    fixed stuff

--- Scripts/missile.gd
+++ Scripts/missile.gd
@@ -4,7 +4,7 @@ extends Area2D
 var animation_player
 
 @export var impact_damage = 0;
-@export var speed = 500;
+@export var speed = 1000;
 @export var cooldown = 0.5;
 @export var lifetime = 1;
 
@@ -14,14 +14,13 @@ var animation_player
 @onready var rootNode = get_node("/root")
 
 func _ready() -> void:
-	#animation_player = $AnimatedSprite2D
-	#animation_player.play()
-	linear_velocity =  Vector2(0, -speed)
+	animation_player = $AnimatedSprite2D
+	animation_player.play()
 	pass
 	
 func _process(delta: float) -> void:
 	print(delta)
-	#position.y -= speed * delta
+	position.y -= speed * delta
 	
 	if(lifetime > 0):
 		lifetime -= delta
